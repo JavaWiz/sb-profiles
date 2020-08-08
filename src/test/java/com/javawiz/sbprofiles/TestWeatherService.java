b8cf35d3f8d416441974f77d91690d3c5b454c2a
Ranjan Sai <mail2javawiz@gmail.com>
2020-08-09 00:14:01 +0530

    update project

--- src/test/java/com/javawiz/sbprofiles/TestWeatherService.java
+++ src/test/java/com/javawiz/sbprofiles/TestWeatherService.java
@@ -1,11 +1,9 @@
 package com.javawiz.sbprofiles;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
